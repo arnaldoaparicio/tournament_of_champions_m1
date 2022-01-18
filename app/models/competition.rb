68eb9ff5413af17ac8fadc563292abfde757d190
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-18 12:44:28 -0500

    Add average_age method to calculate age of all teams players in competition

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,5 +1,9 @@
 class Competition < ApplicationRecord
-has_many :competition_teams
-has_many :teams, through: :competition_teams
+  has_many :competition_teams
+  has_many :teams, through: :competition_teams
 
+
+  def average_age
+    teams.joins(:players).average(:age).round
+  end
 end
