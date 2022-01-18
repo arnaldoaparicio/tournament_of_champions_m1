c0c7375a1cdf455a31b374cde8d01f21ba0cf317
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-18 12:46:31 -0500

    Add code to before each statement

--- spec/features/competitions/index_spec.rb
+++ spec/features/competitions/index_spec.rb
@@ -5,6 +5,20 @@ RSpec.describe 'Competition index page' do
     @comp1 = Competition.create!(name: 'Mens Regional', location: 'Louisville', sport: 'basketball')
     @comp2 = Competition.create!(name: 'Womens Regional', location: 'Rutgersville', sport: 'soccer')
     @comp3 = Competition.create!(name: 'Kids Regional', location: 'Cumberland', sport: 'hockey')
+
+    @team1 =  Team.create!(nickname: 'Rockets', hometown: 'Leesburg')
+    @team2 =  Team.create!(nickname: 'Aristocrats', hometown: 'Smithville')
+    @team3 =  Team.create!(nickname: 'Dreamers', hometown: 'Chicago')
+
+    @player1 = @team1.players.create!(name: 'James Mitchell', age: 20)
+    @player2 = @team1.players.create!(name: 'Bob Sapp', age: 31)
+    @player3 = @team1.players.create!(name: 'Don Frye', age: 26)
+    @player4 = @team2.players.create!(name: 'Joe Schmoe', age: 21)
+    @player5 = @team2.players.create!(name: 'Karl Anderson', age: 40)
+    @player6 = @team2.players.create!(name: 'Terry Gordy', age: 47)
+
+    CompetitionTeam.create!(team: @team1, competition: @comp1)
+    CompetitionTeam.create!(team: @team2, competition: @comp1)
   end
 
 
